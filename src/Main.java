07d586a63dc594882ceb44f9f394774a75bfde6b
Aleksey <kolesov000@mail.ru>
2024-12-12 23:39:21 +0700

    final commit

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,14 @@
 public class Main {
     public static void main(String[] args) {
         int onTheAccount = 500; // на счету
-        int upTheAccount = 1700; // пополнение счета
+        int upTheAccount = 900; // пополнение счета
         int bonus;
         if (upTheAccount > 1000) {
             bonus = upTheAccount / 100;
         } else {
             bonus = 0;
         }
-int result = onTheAccount + upTheAccount + bonus;
+        int result = onTheAccount + upTheAccount + bonus;
         System.out.println(result + " рублей на счету, включая " + bonus + " бонусных рублей ");
     }
 }
